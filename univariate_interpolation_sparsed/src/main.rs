2e65e01121e3d01091db480db2297ef2d754608c
casweeney <casweeno2000@gmail.com>
2025-01-17 07:26:37 +0100

    wip: impl polynomial addition

--- univariate_interpolation_sparsed/src/main.rs
+++ univariate_interpolation_sparsed/src/main.rs
@@ -30,19 +30,49 @@ impl UnivariatePolynomialSparsed {
     
         result
     }
+}
 
-    fn interpolate(&self) {
-    
+fn add_polynomials(polynomial1: &Vec<(u32, u32)>, polynomial2: &Vec<(u32, u32)>) -> Vec<(u32, u32)> {
+    let mut resolved_polynomial = polynomial1.clone();
+
+    // merge both polynomials into one vector
+    resolved_polynomial.extend(polynomial2.clone());
+
+    // resolve polynomials by adding the coefficients with same exponent(power)
+    simplify_terms(&mut resolved_polynomial);
+
+    resolved_polynomial
+}
+
+fn simplify_terms(terms: &mut Vec<(u32, u32)>) {
+    terms.sort_by_key(|points| points.1);
+
+    let mut i = 0;
+
+    while i < terms.len() - 1 {
+        if terms[i].1 == terms[i + 1].1 {
+            // combine coefficients with the same exponents
+            terms[i].0 = terms[i].0.wrapping_add(terms[i + 1].0);
+            terms.remove(i + 1);
+        } else {
+            i += 1;
+        }
     }
+    
+    // remove terms with zero coefficients
+    terms.retain(|&(c, _)| c != 0);
 }
 
 fn main() {
-    let set_of_points = vec!((3,2), (2,1), (5,0));
+    let set_of_points = vec!((2,1), (5,0));
     let evaluated_at = 2;
     let polynomial = UnivariatePolynomialSparsed::new(set_of_points.clone());
 
+    let p1 = vec![(2,1), (5,0)];
+    let p2 = vec![(1,1), (2,0)];
+
     
     println!("Degree of polynomial = {}", polynomial.degree());
-    
     println!("The polynomial of points {:?} evaluated at value {} = {}", set_of_points, evaluated_at, polynomial.evaluate(evaluated_at));
+    println!("Polynomial added: {:?}", add_polynomials(&p1, &p2));
 }
