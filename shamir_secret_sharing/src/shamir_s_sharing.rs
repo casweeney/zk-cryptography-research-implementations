9e270e4744cf90b6d88564ed0db23f3c9bd63eb7
casweeney <casweeno2000@gmail.com>
2025-01-21 00:13:04 +0100

    updated: updated shamir secret sharing to use password

--- shamir_secret_sharing/src/shamir_s_sharing.rs
+++ shamir_secret_sharing/src/shamir_s_sharing.rs
@@ -8,22 +8,27 @@ use ark_ff::PrimeField;
 // using the number of shares, we separated the x and y values
 // Then we interpolated again to get a polynomial which when evaluated at 0, we get back the secret
 
-pub fn s_shares<F: PrimeField>(secret: F, threshold: u64, number_shares: u64) -> Vec<(F, F)> {
-    let mut x_values = vec![F::from(0)];
+/// Added password to serve as an extra level of security.
+/// The password will be the point at which x will be evaluated to get the secret.
+pub fn s_shares<F: PrimeField>(secret: F, password: u64, threshold: u64, number_shares: u64) -> Vec<(F, F)> {
+    let mut x_values = vec![F::from(password)];
     let mut y_values = vec![secret];
 
     let mut rng = rand::thread_rng();
     let mut shares: Vec<(F, F)> = Vec::new();
 
-    for i in 1..threshold {
+    for i in password + 1..threshold {
         x_values.push(F::from(i));
         y_values.push(F::rand(&mut rng));
     }
 
     let polynomial = DensedUnivariatePolynomial::lagrange_interpolate(x_values, y_values);
 
-    for i in 1..number_shares {
-        shares.push((F::from(i), polynomial.evaluate(F::from(i))))
+    // todo!() : We need a while loop to regenerate the random numbers if the condition below fails 
+    if polynomial.degree() as u64 == threshold - 1 {
+        for i in password + 1..number_shares {
+            shares.push((F::from(i), polynomial.evaluate(F::from(i))))
+        }
     }
 
     shares
@@ -53,11 +58,12 @@ mod tests {
 
     #[test]
     fn test_s_recover_secret() {
+        let password = 0;
         let secret = Fq::from(17);
         let threshold = 4;
         let number_of_shares = 10;
 
-        let shares = s_shares(secret, threshold, number_of_shares);
+        let shares = s_shares(secret, password, threshold, number_of_shares);
 
         let recovered_secret = s_recover_secret(shares);
 
@@ -67,11 +73,12 @@ mod tests {
 
     #[test]
     fn test_s_recover_wrong_secret_fails() {
+        let password = 0;
         let secret = Fq::from(17);
         let threshold = 4;
         let number_of_shares = 10;
 
-        let shares = s_shares(secret, threshold, number_of_shares);
+        let shares = s_shares(secret, password, threshold, number_of_shares);
 
         let recovered_secret = s_recover_secret(shares);
 
