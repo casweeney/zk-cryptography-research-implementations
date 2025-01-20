5c7167a376bc32253c23e4a9fb32e2b4b560eaff
casweeney <casweeno2000@gmail.com>
2025-01-20 12:17:39 +0100

    commented sparse code

--- polynomials/univariate/src/sparse_univariate.rs
+++ polynomials/univariate/src/sparse_univariate.rs
@@ -1,18 +1,18 @@
 
-struct SparsedUniVariatePolynomial {
-    values: (f64, f64) // => (x,y) values
-}
+// struct SparsedUniVariatePolynomial {
+//     values: (f64, f64) // => (x,y) values
+// }
 
-impl SparsedUniVariatePolynomial {
-    pub fn degree() -> u32 {
-        todo!()
-    }
+// impl SparsedUniVariatePolynomial {
+//     pub fn degree() -> u32 {
+//         todo!()
+//     }
 
-    pub fn evaluate() -> u32 {
-        todo!()
-    }
+//     pub fn evaluate() -> u32 {
+//         todo!()
+//     }
 
-    pub fn lagrange_interpolate() -> SparsedUniVariatePolynomial {
-        todo!()
-    }
-}
+//     pub fn lagrange_interpolate() -> SparsedUniVariatePolynomial {
+//         todo!()
+//     }
+// }
