1403d0b611f453cb86715d80c7d58561f9400821
casweeney <casweeno2000@gmail.com>
2025-02-03 14:12:57 +0100

    wip: implementation structure setup

--- sumcheck_protocol/src/sumcheck_interactive_simulation.rs
+++ sumcheck_protocol/src/sumcheck_interactive_simulation.rs
@@ -1,9 +1,38 @@
-pub struct Prover {
+use polynomials::multilinear::evaluation_form::{partial_evaluate, MultilinearPolynomial};
+use ark_ff::{PrimeField, BigInteger};
 
+pub struct Prover<F: PrimeField> {
+    pub initial_polynomial: MultilinearPolynomial<F>,
+    pub initial_claimed_sum: F,
 }
 
-pub struct Verifier {
+impl <F: PrimeField>Prover<F> {
+    pub fn new() -> Self {
+        todo!()
+    }
 
+    pub fn prove() {
+
+    }
+}
+
+pub struct Verifier<F: PrimeField> {
+    pub initial_polynomial: MultilinearPolynomial<F>,
+    pub initial_claimed_sum: F,
+}
+
+impl <F: PrimeField>Verifier<F> {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn verify() {
+
+    }
+
+    pub fn oracle_check() {
+        
+    }
 }
 
 #[cfg(test)]
@@ -11,6 +40,6 @@ mod tests {
     use super::*;
 
     fn test_sumcheck_interactive_simulation() {
-        
+
     }
 }
