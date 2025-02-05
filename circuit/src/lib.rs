fd9a2a58c512f0bbaef4163f63a5e60b884b153f
casweeney <casweeno2000@gmail.com>
2025-02-05 23:54:31 +0100

    wip: completed circuit impl

--- circuit/src/lib.rs
+++ circuit/src/lib.rs
@@ -1,33 +1,32 @@
 use ark_ff::PrimeField;
+use std::marker::PhantomData;
 
 pub enum Operator {
     Add,
     Mul
 }
 
-pub struct Gate<F: PrimeField> {
-    pub output: F,
-    pub left_input: F,
-    pub right_input: F,
+pub struct Gate {
+    pub left_index: usize,
+    pub right_index: usize,
     pub operator: Operator
 }
 
-pub struct Layer<F: PrimeField> {
-    pub gates: Vec<Gate<F>>
+pub struct Layer {
+    pub gates: Vec<Gate>
 }
 
 pub struct Circuit<F: PrimeField> {
-    pub layers: Vec<Layer<F>>
+    pub layers: Vec<Layer>,
+    _phantom: PhantomData<F>
 }
 
 //////////////// Gate Implementation ///////////////////
-
-impl <F: PrimeField>Gate<F> {
-    pub fn new(left_input: F, right_input: F, operator: Operator) -> Self {
+impl Gate {
+    pub fn new(left_index: usize, right_index: usize, operator: Operator) -> Self {
         Self {
-            output: evaluate_inputs(left_input, right_input, &operator),
-            left_input,
-            right_input,
+            left_index,
+            right_index,
             operator
         }
     }
@@ -41,8 +40,8 @@ pub fn evaluate_inputs<F: PrimeField>(left: F, right: F, operator: &Operator) ->
 }
 
 /////////////// Layer implementation ////////////////////
-impl <F: PrimeField>Layer<F> {
-    pub fn new(gates: Vec<Gate<F>>) -> Self {
+impl Layer {
+    pub fn new(gates: Vec<Gate>) -> Self {
         Self {
             gates
         }
@@ -53,33 +52,31 @@ impl <F: PrimeField>Layer<F> {
 //////////////// Circuit Implementation ///////////////////
 
 impl <F: PrimeField>Circuit<F> {
-    pub fn new(layers: Vec<Layer<F>>) -> Self {
+    pub fn new(layers: Vec<Layer>) -> Self {
         Self {
-            layers
+            layers,
+            _phantom: PhantomData
         }
     }
-    
-    pub fn execute(values: Vec<F>) {
 
-    }
-}
+    pub fn evaluate(&self, values: Vec<F>) -> F {
+        let mut current_input = values;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ark_bn254::Fq;
+        for i in 0..self.layers.len() {
+            let mut resultant_evaluations = Vec::new();
 
-    #[test]
-    fn test_gate_add_operator() {
-        let gate = Gate::new(Fq::from(5), Fq::from(3), Operator::Add);
+            for j in 0..self.layers[i].gates.len() {
+                let current_gate_evaluation = match self.layers[i].gates[j].operator {
+                    Operator::Add => current_input[self.layers[i].gates[j].left_index] + current_input[self.layers[i].gates[j].right_index],
+                    Operator::Mul => current_input[self.layers[i].gates[j].left_index] * current_input[self.layers[i].gates[j].right_index]
+                };
 
-        assert_eq!(gate.output, Fq::from(8));
-    }
+                resultant_evaluations.push(current_gate_evaluation);
+            }
 
-    #[test]
-    fn test_gate_mul_operator() {
-        let gate = Gate::new(Fq::from(5), Fq::from(3), Operator::Mul);
+            current_input = resultant_evaluations;
+        }
 
-        assert_eq!(gate.output, Fq::from(15));
+        current_input[0]
     }
 }
