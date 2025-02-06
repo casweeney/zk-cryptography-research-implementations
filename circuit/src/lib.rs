92b2c70f3f35df29083e897f1ecf6dc86c34ccfc
casweeney <casweeno2000@gmail.com>
2025-02-06 10:49:27 +0100

    mod: removed loop over number range

--- circuit/src/lib.rs
+++ circuit/src/lib.rs
@@ -55,28 +55,9 @@ impl <F: PrimeField>Circuit<F> {
     pub fn evaluate(&self, values: Vec<F>) -> F {
         let mut current_input = values;
 
-        // Using Loops
-        // for i in 0..self.layers.len() {
-        //     let mut resultant_evaluations = Vec::new();
-
-        //     for j in 0..self.layers[i].gates.len() {
-        //         let left_index_value = current_input[self.layers[i].gates[j].left_index];
-        //         let right_index_value = current_input[self.layers[i].gates[j].right_index];
-
-        //         let current_gate_evaluation = match self.layers[i].gates[j].operator {
-        //             Operator::Add => left_index_value + right_index_value,
-        //             Operator::Mul => left_index_value * right_index_value
-        //         };
-
-        //         resultant_evaluations.push(current_gate_evaluation);
-        //     }
-
-        //     current_input = resultant_evaluations;
-        // }
-
         // Using Iterators
         for layer in self.layers.iter() {
-            let mut resultant_evaluations = Vec::new();
+            let mut resultant_evaluations = Vec::new(); // vec num of gates
 
             for gate in layer.gates.iter() {
                 let left_index_value = current_input[gate.left_index];
