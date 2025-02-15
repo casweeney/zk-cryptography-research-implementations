fc6bf2d461f52ff4a982f2ddc35411457c9e1194
casweeney <casweeno2000@gmail.com>
2025-02-15 01:29:39 +0100

    wip: implemented evaluate_product_poly()

--- polynomials/src/composed/composed_multilinear.rs
+++ polynomials/src/composed/composed_multilinear.rs
@@ -16,17 +16,36 @@ impl <F: PrimeField>ProductPolynomial<F> {
         }
     }
     
-    pub fn evaluate_product_poly(polynomials: Vec<MultilinearPolynomial<F>>, values: Vec<F>) -> F {
+    pub fn evaluate_product_poly(&self, values: &Vec<F>) -> F {
         let mut result = F::one();
 
-        for polynomial in polynomials.iter() {
+        for polynomial in self.polynomials.iter() {
             result *= polynomial.evaluate(&values);
         }
 
         result
     }
     
-    pub fn partial_evaluate_product_poly(polynomials: Vec<MultilinearPolynomial<F>>) {
+    pub fn partial_evaluate_product_poly(polynomials: Vec<MultilinearPolynomial<F>>) -> Vec<MultilinearPolynomial<F>> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fq;
+
+    #[test]
+    fn test_evaluate_product_poly() {
+        let polynomail1 = MultilinearPolynomial::new(&vec![Fq::from(0), Fq::from(0), Fq::from(0), Fq::from(2)]);
+        let polynomail2 = MultilinearPolynomial::new(&vec![Fq::from(0), Fq::from(0), Fq::from(0), Fq::from(3)]);
+
+        let polynomials = vec![polynomail1, polynomail2];
+
+        let product_polynomial = ProductPolynomial::new(polynomials);
+        let values = vec![Fq::from(1), Fq::from(2)];
+
+        assert_eq!(product_polynomial.evaluate_product_poly(&values), Fq::from(24));
     }
 }
