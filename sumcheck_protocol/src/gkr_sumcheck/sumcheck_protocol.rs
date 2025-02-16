61194814588babb6eac917555c69df84886cdcb9
casweeney <casweeno2000@gmail.com>
2025-02-16 01:27:39 +0100

    wip: added degree() to get degree of composed polynomial

---
+++ sumcheck_protocol/src/gkr_sumcheck/sumcheck_protocol.rs
@@ -0,0 +1,7 @@
+use polynomials::composed::composed_multilinear::ComposedPolynomial;
+use transcripts::fiat_shamir::fiat_shamir_transcript::Transcript;
+use ark_ff::{PrimeField, BigInteger};
+
+pub struct GKRSumcheck {
+    
+}
