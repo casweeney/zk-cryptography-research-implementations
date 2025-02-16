8e3b2476ab48f46372948d7592ecc9804ae26aa3
casweeney <casweeno2000@gmail.com>
2025-02-16 02:26:26 +0100

    wip: impl gkr sumcheck

--- sumcheck_protocol/src/gkr_sumcheck/sumcheck_protocol.rs
+++ sumcheck_protocol/src/gkr_sumcheck/sumcheck_protocol.rs
@@ -1,7 +1,40 @@
 use polynomials::composed::composed_multilinear::ComposedPolynomial;
-use transcripts::fiat_shamir::fiat_shamir_transcript::Transcript;
+use transcripts::fiat_shamir::{fiat_shamir_transcript::Transcript, interface::FiatShamirTranscriptInterface};
 use ark_ff::{PrimeField, BigInteger};
 
-pub struct GKRSumcheck {
-    
+pub struct GKRSumcheck<F: PrimeField> {
+    pub polynomial: ComposedPolynomial<F>,
+    pub claimed_sum: F
+}
+
+pub struct GKRSumcheckProof<F: PrimeField> {
+    pub polynomial: ComposedPolynomial<F>,
+    pub round_univariate_polynomials: Vec<Vec<F>>
+}
+
+impl <F: PrimeField>GKRSumcheck<F> {
+    pub fn init(polynomial: ComposedPolynomial<F>) -> Self {
+        let claimed_sum = Self::calculate_product_poly_sum(&polynomial);
+
+        Self {
+            polynomial,
+            claimed_sum
+        }
+    }
+
+    pub fn calculate_product_poly_sum(polynomial: &ComposedPolynomial<F>) -> F {
+        polynomial.multiply_polynomials_element_wise().evaluated_values.iter().sum()
+    }
+
+    pub fn prove() -> GKRSumcheckProof<F> {
+        let transcript = Transcript::new();
+
+        todo!()
+    }
+
+    pub fn verify() -> bool {
+        let transcript = Transcript::new();
+
+        todo!()
+    }
 }
