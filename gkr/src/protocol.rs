bd9520b5c3b94f52085e2a19008fc6f29882474f
casweeney <casweeno2000@gmail.com>
2025-02-18 20:56:34 +0100

    wip: gkr protocol structure init

---
+++ gkr/src/protocol.rs
@@ -0,0 +1,15 @@
+use circuit::arithmetic_circuit::Circuit;
+use sumcheck_protocol::gkr_sumcheck::gkr_sumcheck_protocol::GKRSumcheck;
+use polynomials::composed::{product_polynomial, sum_polynomial};
+
+pub struct Proof {
+
+}
+
+pub fn prove() {
+
+}
+
+pub fn verify() {
+    
+}
