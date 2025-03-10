1a4bf589ad8c9fc26f005bb89748a9298b7b1bbe
casweeney <casweeno2000@gmail.com>
2025-03-11 00:28:35 +0100

    wip: implemented multilinear kzg trusted setup

---
+++ kzg/src/multilinear_kzg.rs
@@ -0,0 +1,19 @@
+
+pub fn commit_to_polynomial() {
+
+}
+
+pub fn generate_proof() {
+
+}
+
+pub fn verify() {
+
+}
+
+// This is a helper function to generate random numbers as values of tau, based on the number of variables
+pub fn generate_taus(no_of_variables: usize) -> Vec<usize> {
+    let mut values_of_tau: Vec<usize> = Vec::with_capacity(no_of_variables);
+
+    todo!()
+}
