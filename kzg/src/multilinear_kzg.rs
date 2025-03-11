551b484c5318c650bbf29f4ed775688c23166519
casweeney <casweeno2000@gmail.com>
2025-03-11 20:35:43 +0100

    wip: implemented function to commit to polynomial

--- kzg/src/multilinear_kzg.rs
+++ kzg/src/multilinear_kzg.rs
@@ -1,14 +1,46 @@
+use ark_ff::PrimeField;
+use ark_ec::{pairing::Pairing, PrimeGroup};
+use polynomials::multilinear::evaluation_form::MultilinearPolynomial;
+use crate::trusted_setup::TrustedSetup;
 
-pub fn commit_to_polynomial() {
-
+pub struct MultilinearKZGProof<F: PrimeField, P: Pairing> {
+    pub evaluation: F,
+    pub proofs: Vec<P::G1>
 }
 
-pub fn generate_proof() {
 
+pub fn commit_to_polynomial<F: PrimeField, P: Pairing>(
+    polynomial: MultilinearPolynomial<F>,
+    trust_setup: TrustedSetup<P>
+) -> P::G1 {
+    assert_eq!(polynomial.evaluated_values.len(), trust_setup.g1_powers_of_tau.len(), "Polynomial evaluation must match g1 length");
+
+    let commitment = polynomial.evaluated_values
+        .iter()
+        .zip(trust_setup.g1_powers_of_tau.iter())
+        .map(|(coefficient, power)| power.mul_bigint(coefficient.into_bigint()))
+        .sum();
+
+    commitment
 }
 
-pub fn verify() {
+pub fn prove<F: PrimeField, P: Pairing>(
+    polynomial: MultilinearPolynomial<F>,
+    trust_setup: TrustedSetup<P>,
+    opening_values: &[F]
+) -> MultilinearKZGProof<F, P> {
 
+    todo!()
+}
+
+pub fn verify<F: PrimeField, P: Pairing>(
+    trust_setup: TrustedSetup<P>,
+    commitment: &P::G1,
+    opening_values: &[F],
+    proof: MultilinearKZGProof<F, P>
+) -> bool {
+
+    todo!()
 }
 
 // This is a helper function to generate random numbers as values of tau, based on the number of variables
