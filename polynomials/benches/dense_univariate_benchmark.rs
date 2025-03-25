a76016446e16819f87434b042c8f2dc160d1c9e6
casweeney <casweeno2000@gmail.com>
2025-03-25 15:36:06 +0100

    mod: removed criterion benches from polynomial

--- polynomials/benches/dense_univariate_benchmark.rs
+++
@@ -1,8 +0,0 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-
-fn dense_univariate_benchmark(c: &mut Criterion) {
-    // Your benchmark code goes here
-}
-
-criterion_group!(benches, dense_univariate_benchmark);
-criterion_main!(benches);
