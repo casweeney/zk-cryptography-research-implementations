596f08a485f73831d5560328aa5cfbdd262fbbf5
casweeney <casweeno2000@gmail.com>
2025-03-25 14:59:37 +0100

    test: added benchmarking to test field element ops count

--- polynomials/benches/dense_univariate_benchmark.rs
+++ polynomials/benches/dense_univariate_benchmark.rs
@@ -1,8 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn densed_univariate_benchmark(c: &mut Criterion) {
+fn dense_univariate_benchmark(c: &mut Criterion) {
     // Your benchmark code goes here
 }
 
-criterion_group!(benches, densed_univariate_benchmark);
+criterion_group!(benches, dense_univariate_benchmark);
 criterion_main!(benches);
